3c8c68fe84b491f7898b1b32fc70ab98ef739f23
Дима <@DESKTOP-TC925PR>
2022-11-30 23:12:07 +0200

    Add project files.

---
+++ 9.11/9.11.cpp
@@ -0,0 +1,97 @@
+﻿#include <windows.h>
+#include <tchar.h>
+#include <time.h>
+#include "resource.h"
+
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+
+HWND hList, hButton, hEdit;
+
+TCHAR str[50];
+TCHAR str_radio_box1[50];
+TCHAR str_radio_box2[50];
+TCHAR str_radio_box3[50];
+
+int rand_time = 0, min = 10, max = 20;
+int num = 0, num_min = -10, num_max = 10;
+int mas[50];
+long long result = 0;
+
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
+{
+	return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
+}
+
+void RandNum() {
+	num = num_min + rand() % (num_max - num_min + 1);
+	if (num == 0) {
+		num = num_min + rand() % (num_max - num_min + 1);
+	}
+	wsprintf(str, TEXT("%d"), num);
+}
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	srand(time(0));
+	switch (message)
+	{
+	case WM_INITDIALOG: {
+		hList = GetDlgItem(hWnd, IDC_LIST1);
+		hButton = GetDlgItem(hWnd, IDC_BUTTON1);
+		hEdit = GetDlgItem(hWnd, IDC_EDIT1);
+	}
+					  break;
+
+	case WM_COMMAND: {
+		if (LOWORD(wParam) == IDC_BUTTON1) {
+			SendMessage(hList, LB_RESETCONTENT, 0, 0);
+			rand_time = min + rand() % (max - min + 1);
+
+			for (int i = 0; i < rand_time; i++) {
+				SendMessage(hList, LB_ADDSTRING, 0, LPARAM(str));
+				RandNum();
+				mas[i] = num;
+			}
+		}
+
+		if (LOWORD(wParam) == IDC_RADIO1) {
+			for (int i = 0; i < rand_time - 1; i++) {
+				result += mas[i];
+			}
+			wsprintf(str_radio_box1, TEXT("%d"), result);
+			SendMessage(hEdit, WM_SETTEXT, 0, LPARAM(str_radio_box1));
+			result = 0;
+		}
+
+		if (LOWORD(wParam) == IDC_RADIO2) {
+			result = mas[0];
+			for (int i = 1; i < rand_time - 1; i++) {
+				result *= mas[i];
+			}
+			wsprintf(str_radio_box2, TEXT("%d"), result);
+			SendMessage(hEdit, WM_SETTEXT, 0, LPARAM(str_radio_box2));
+			result = 0;
+		}
+
+		if (LOWORD(wParam) == IDC_RADIO3) {
+			for (int i = 0; i < rand_time - 1; i++) {
+				result += mas[i];
+			}
+			result /= rand_time;
+
+			double rez = 123.23;
+
+			wsprintf(str_radio_box3, TEXT("%d"), result);
+			SendMessage(hEdit, WM_SETTEXT, 0, LPARAM(str_radio_box3));
+			result = 0;
+		}
+
+	}
+				   break;
+
+	case WM_CLOSE:
+		EndDialog(hWnd, 0);
+		return TRUE;
+	}
+	return FALS
---
+++ 9.11/Resource.h
@@ -0,0 +1,22 @@
+﻿//{{NO_DEPENDENCIES}}
+// Âêëþ÷àåìûé ôàéë, ñîçäàííûé â Microsoft Visual C++.
+// Èñïîëüçóåòñÿ 09.11.2022 homework.rc
+//
+#define IDD_DIALOG1                     101
+#define IDC_LIST1                       1001
+#define IDC_BUTTON1                     1003
+#define IDC_EDIT1                       1004
+#define IDC_RADIO1                      1005
+#define IDC_RADIO2                      1006
+#define IDC_RADIO3                      1007
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
